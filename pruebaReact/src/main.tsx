5c4cb9720771b2098f864b904cae1437ee5d2850
pablocasamianestudios <pablocasamianestudios@gmail.com>
2025-11-10 10:27:15 +0100

    ADD options and select component

--- pruebaReact/src/main.tsx
+++ pruebaReact/src/main.tsx
@@ -6,12 +6,14 @@ import App from './App.tsx'
 import Button from './components/Button/Button.tsx'
 import Encabezado1 from './components/Encabezado1/Encabezado1';
 import Image from './components/Image/Image';
+import Select from './components/Select/Select.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Encabezado1 />
     <Button />
     <Image />
+    <Select />
     {/* <App /> */}
   </StrictMode>,
 )
